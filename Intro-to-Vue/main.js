572854d738073627a3778ab3adfc42cb325c0e0a
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-08-15 01:55:02 +0500

    8 lesson finish

--- Intro-to-Vue/main.js
+++ Intro-to-Vue/main.js
@@ -1,19 +1,27 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Shoes",
-      image: "./assets/images/socks_green.jpg",
-      inStock: true,
-      quantity: 36,
       cart: 0,
+      product: "Socks",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
-      // solution
-      sizes: ["S", "M", "L", "XL"],
-      // solution
       variants: [
-        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 2234,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+        },
+        {
+          id: 2235,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+        },
       ],
+      // solution
+      onSale: true,
     };
   },
   methods: {
@@ -22,6 +30,26 @@ const app = Vue.createApp({
     },
     changeImage(variantImage) {
       this.image = variantImage;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    saleMessage() {
+      if (this.onSale) {
+        return this.brand + " " + this.product + " is on sale.";
+      }
+      return "";
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
     },
   },
 });
