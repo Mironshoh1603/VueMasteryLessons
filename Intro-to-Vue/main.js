ff6d6c01afcaf331a1cc363aa5fadf40de06c216
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-08-14 20:46:41 +0500

    Intro lesson 6 is finished

--- Intro-to-Vue/main.js
+++ Intro-to-Vue/main.js
@@ -8,15 +8,24 @@ const app = Vue.createApp({
       image: "./assets/images/socks_green.jpg",
       stock: true,
       quantity: 36,
+      cart: 0,
       details: ["50% cotton", "30% wool", "20% polyester"],
       // solution
       sizes: ["S", "M", "L", "XL"],
       // solution
       variants: [
-        { id: 2234, color: "green" },
-        { id: 2235, color: "blue" },
+        { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+        { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
       ],
     };
+  },
+  methods: {
+    addCart() {
+      this.cart += 1;
+    },
+    changeImage(variantImage) {
+      this.image = variantImage;
+    },
   },
 });
 
