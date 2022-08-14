1d2b2a8512eae197ff29aea91bcdd40344ae880e
mironshoh1603 <mironshohasadov1603@gmail.com>
2022-08-14 20:31:22 +0500

    lesson 5 finished

--- Intro-to-Vue/main.js
+++ Intro-to-Vue/main.js
@@ -8,6 +8,14 @@ const app = Vue.createApp({
       image: "./assets/images/socks_green.jpg",
       stock: true,
       quantity: 36,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      // solution
+      sizes: ["S", "M", "L", "XL"],
+      // solution
+      variants: [
+        { id: 2234, color: "green" },
+        { id: 2235, color: "blue" },
+      ],
     };
   },
 });
